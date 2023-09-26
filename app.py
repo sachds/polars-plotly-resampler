313a3f510b28a2884d0602548faf5e76f81359a4
sachds <root@noshadeson>
2023-09-25 23:25:27 -0400

    polars requirement final

--- app.py
+++ app.py
@@ -78,11 +78,11 @@ from sqlalchemy import create_engine
 
 # SQL statements (unchanged)
 
-engine_temp_stmt = "SELECT Timestamp, EngineTemperature_C FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
-oil_pressure_stmt = "SELECT Timestamp, OilPressure_psi FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
-speed_stmt = "SELECT Timestamp, 'Speed_km/h' FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
-tire_pressure_stmt = "SELECT Timestamp, TirePressure_psi FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
-battery_voltage_stmt = "SELECT Timestamp, BatteryVoltage_V FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
+engine_temp_stmt = "SELECT Timestamp, EngineTemperature FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
+oil_pressure_stmt = "SELECT Timestamp, OilPressure FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
+speed_stmt = "SELECT Timestamp, Speed FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
+tire_pressure_stmt = "SELECT Timestamp, TirePressure FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
+battery_voltage_stmt = "SELECT Timestamp, BatteryVoltage FROM main.resamplerdata.auto_iot_bronze_sensors LIMIT 1000000;"
 
 
 # Read data from SQL queries into Polars dataframes
